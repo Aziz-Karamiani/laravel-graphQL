7bf36b47367e7c3e246a626cd1c75e7c3e0ba6fc
a.karamiani <aziz.karamiani@gmail.com>
2022-11-08 22:25:06 +0330

    Database Design

--- database/migrations/2022_11_08_184743_create_tasks_table.php
+++ database/migrations/2022_11_08_184743_create_tasks_table.php
@@ -13,14 +13,13 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('tasks', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->string('api_token')->unique();
-            $table->rememberToken();
+            $table->text("title");
+            $table->text("description");
+            $table->tinyInteger("Status");
+            $table->foreignId('user_id')->constrained();
+            $table->foreignId('project_id')->constrained();
             $table->timestamps();
         });
     }
@@ -32,6 +31,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('tasks');
     }
 };
